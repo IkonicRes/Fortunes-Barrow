c64231a28378763ec671c676ed3f350250022aa7
IkonicResonance <67353747+IkonicResonance@users.noreply.github.com>
2023-06-10 03:48:42 -0500

    switched to premade layouts, made level one and player controller/charactermovement

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,29 +1,5 @@
-class MainScene extends Phaser.Scene {
-    preload() {
-        console.log('Preload method called.');
-        let tileSet = get_asset_url('../images/tileset/dungeon_.png');
-        console.log('Tileset URL:', tileSet);
-        this.load.image('tiles', tileSet);
-      }
-  
-    create() {
-      const mapData = generateDungeon(); // Generate the dungeon layout
-      const map = this.make.tilemap({ data: mapData, tileWidth: 16, tileHeight: 16 });
-      const tiles = map.addTilesetImage('tiles');
-      const layer = map.createLayer(0, tiles, 0, 0); // Create a layer using createLayer instead
-      layer.setCollisionByExclusion([-1]); // Optional: Set collision properties
-  
-      // Rest of your scene code
-    }
-  }
 
-function get_asset_url(assetPath) {
-    // Replace this with your logic to get the correct URL
-    const baseUrl = '';
-    return baseUrl + assetPath;
-  }
-
-//All dice fuctions
+const baseDir = window.location.pathname.substring(0, window.location.pathname.lastIndexOf("/"));
 const r1d4 = () => Math.ceil(Math.random() * 4)
 const r1d6 = () => Math.ceil(Math.random() * 6)
 const r1d8 = () => Math.ceil(Math.random() * 8)
@@ -32,45 +8,135 @@ const r1d12 = () => Math.ceil(Math.random() * 12)
 const r1d20 = () => Math.ceil(Math.random() * 20)
 const r1d100 = () => Math.ceil(Math.random() * 100)
 
+function get_asset_url(assetPath) {
+  console.log(baseDir)
+  console.log(baseDir + assetPath)
+  return baseDir + assetPath;
+}
 
-// Random walk dungeon generation
-function generateDungeon() {
-    const width = 40;
-    const height = 30;
-    const maxWalkSteps = 1000;
+class startLevelOne extends Phaser.Scene {
+  preload() {
+    this.load.tilemapTiledJSON('level', get_asset_url('/assets/images/tileset/levels/L_01/L_01.json'));
+    this.load.spritesheet('fantasyDungeonTilesetTransparent', '../assets/images/tileset/dungeonTiles/fantasyDungeonTilesetTransparent.png', { frameWidth: 16, frameHeight: 16 });
+    this.load.spritesheet('character', '../assets/images/characters/T_char.png', { frameWidth: 32, frameHeight: 32 });
+  }
 
-    // Initialize an empty dungeon filled with walls
-    const dungeon = Array(height).fill(0).map(() => Array(width).fill(1));
+  create() {
+    const map = this.make.tilemap({ key: 'level' });
+    const tileset = map.addTilesetImage('fantasyDungeonTilesetTransparent', 'fantasyDungeonTilesetTransparent');
 
-    let x = Math.floor(Math.random() * width);
-    let y = Math.floor(Math.random() * height);
 
-    for(let i = 0; i < maxWalkSteps; i++) {
-        dungeon[y][x] = 0; // Mark the cell as a floor
+    const terrainLayer = map.createLayer('Terrain', tileset, 0, 0);
+    const overlayLayer = map.createLayer('Overlay', tileset, 0, 0);
+    const collisionLayer = map.createLayer('Collision', tileset, 0, 0);
+    collisionLayer.setVisible(true);
+    var scale = 3
+    var playerScale = 2
+    terrainLayer.setScale(scale);
+    collisionLayer.setScale(scale);
+    overlayLayer.setScale(scale);
 
-        const direction = Math.floor(Math.random() * 4);
-        switch(direction) {
-            case 0: x = Math.max(0, x - 1); break; // left
-            case 1: x = Math.min(width - 1, x + 1); break; // right
-            case 2: y = Math.max(0, y - 1); break; // up
-            case 3: y = Math.min(height - 1, y + 1); break; // down
+    map.setCollisionByExclusion([], true, collisionLayer); // Set collision on the collision layer
+
+    var playerStart = [75, 75];
+    const character = this.physics.add.sprite(playerStart[0] * 16, playerStart[1] * 16, 'character');
+    character.setScale(playerScale);
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+    this.cameras.main.startFollow(character);
+
+    const speed = 32;
+    const movementDelay = 100; // Delay between each movement step in milliseconds
+
+    let leftTimer, rightTimer, upTimer, downTimer;
+
+    // Set the player controller to handle character movement
+    const playerController = {
+      update: () => {
+        const prevX = character.x;
+        const prevY = character.y;
+
+        // Movement logic for each arrow key
+        const moveLeft = () => {
+          character.x -= speed;
+          if (this.physics.world.collide(character, collisionLayer)) {
+            character.x = prevX;
+          }
+          leftTimer = this.time.delayedCall(movementDelay, moveLeft);
+        };
+
+        const moveRight = () => {
+          character.x += speed;
+          if (this.physics.world.collide(character, collisionLayer)) {
+            character.x = prevX;
+          }
+          rightTimer = this.time.delayedCall(movementDelay, moveRight);
+        };
+
+        const moveUp = () => {
+          character.y -= speed;
+          if (this.physics.world.collide(character, collisionLayer)) {
+            character.y = prevY;
+          }
+          upTimer = this.time.delayedCall(movementDelay, moveUp);
+        };
+
+        const moveDown = () => {
+          character.y += speed;
+          if (this.physics.world.collide(character, collisionLayer)) {
+            character.y = prevY;
+          }
+          downTimer = this.time.delayedCall(movementDelay, moveDown);
+        };
+
+        if (this.cursors.left.isDown && !leftTimer) {
+          moveLeft();
+        } else if (!this.cursors.left.isDown && leftTimer) {
+          leftTimer.remove();
+          leftTimer = null;
         }
-    }
 
-    return dungeon;
+        if (this.cursors.right.isDown && !rightTimer) {
+          moveRight();
+        } else if (!this.cursors.right.isDown && rightTimer) {
+          rightTimer.remove();
+          rightTimer = null;
+        }
+
+        if (this.cursors.up.isDown && !upTimer) {
+          moveUp();
+        } else if (!this.cursors.up.isDown && upTimer) {
+          upTimer.remove();
+          upTimer = null;
+        }
+
+        if (this.cursors.down.isDown && !downTimer) {
+          moveDown();
+        } else if (!this.cursors.down.isDown && downTimer) {
+          downTimer.remove();
+          downTimer = null;
+        }
+      },
+    };
+    // Add the player controller to the scene's update list
+    this.events.on('update', playerController.update);
+  }
 }
 
 const config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    scene: MainScene
+  type: Phaser.AUTO,
+  width: 1280,
+  height: 720,
+  scene: startLevelOne,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false // Set to true for collision debugging
+    }
+  }
 };
 
 const game = new Phaser.Game(config);
-
-
-
 
 console.log(r1d4())
 console.log(r1d6())
@@ -117,6 +183,9 @@ $(document).ready(function() {
     });
   });
 
+
+
+/// DND Fetch calls ///
 function printData(monsterCurrentData) {
   console.log(monsterCurrentData.hit_dice)
   // console.log(monsterCurrentData.hit_points)
